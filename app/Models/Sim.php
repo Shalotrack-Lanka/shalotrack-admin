51586692a5a6337e4faad7dc103c6cbc1653e49a
Amodamendis <amodarashmika2003@gmail.com>
2026-07-09 00:36:36 +0530

    Add canceled_at datetime cast to Sim model

--- app/Models/Sim.php
+++ app/Models/Sim.php
@@ -9,10 +9,12 @@ class Sim extends Model
 {
     use HasFactory;
 
-    // සියලුම fields වලට දත්ත ඇතුළත් කිරීමට අවසර දීම
     protected $guarded = [];
 
-    // Master Stock එක සමඟ ඇති සබඳතාවය
+    protected $casts = [
+        'canceled_at' => 'datetime',
+    ];
+
     public function stock()
     {
         return $this->belongsTo(Stock::class, 'stock_id');
