46102d002c468379229a100992f115624fc32be2
nuwan123-png <dinidunuwanakalanka@gmail.com>
2026-07-12 21:19:25 +0530

    Admin reports generation ( Not Activated sims)

--- routes/web.php
+++ routes/web.php
@@ -120,7 +120,13 @@ Route::middleware(['auth'])->group(function () {
         [CancelDeviceController::class, 'exportActivated'])
         ->name('admin.cancel-device.export-activated');
 
- 
+        Route::get('/add-sim/not-activated/export', 
+        [AddSimController::class, 'exportNotActivated'])
+        ->name('admin.sim.export-not-activated');
+
+       // Route::get('/add-sim/activated/export', 
+       // [AddSimController::class, 'exportActivated'])
+       // ->name('admin.sim.export-activated');
 
     });
 
