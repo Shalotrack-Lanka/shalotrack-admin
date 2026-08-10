2e4f9083f8f41fd0056241fe96731035b4db7fb3
Amodamendis <amodarashmika2003@gmail.com>
2026-08-11 03:14:28 +0530

    Subscription lifecycle: schedule devices:expire-subscriptions to run every minute

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('customers:sync')->everyFiveMinutes();
+Schedule::command('devices:expire-subscriptions')->everyMinute();
